67e6e95cc5ef2dcf89fa1706b113edfedc8ecb62
zhangkang <zhangkang@lvmama.com>
2017-08-04 17:50:32 +0800

    设置jdk版本

--- javaTest/src/test/java/org/zk/aop/JdkAopTest.java
+++ javaTest/src/test/java/org/zk/aop/JdkAopTest.java
@@ -2,9 +2,14 @@ package org.zk.aop;
 
 import org.testng.annotations.Test;
 import org.zhangkang.commons.utils.ClassUtils;
+import org.zhangkang.entity.User;
 import org.zk.aop.jdk.MyInvocationHandler;
 import org.zk.aop.jdk.MyInvocationHandler2;
+import sun.misc.ProxyGenerator;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 /**
@@ -13,14 +18,22 @@ import java.lang.reflect.Proxy;
 public class JdkAopTest {
 
     @Test
-    public void testProxyInterface (){
+    public void testProxyInterface () throws Exception{
         UserDao proxy = (UserDao)Proxy.newProxyInstance(UserDao.class.getClassLoader(),
                 new Class[]{UserDao.class, EmptyInterface.class},
                 new MyInvocationHandler());
         System.out.println(proxy.deleteById(10));
         System.out.println(proxy.getClass());
-        ClassUtils.showInherit(proxy.getClass());
+//        ClassUtils.showInherit(proxy.getClass());
+//        Class c = proxy.getClass();
+//        Method[] methods = c.getMethods();
+//        for (int i = 0; i < methods.length; i++) {
+//            System.out.println(methods[i]);
+//        }
+      // TODO 将字节码保存到本地，用jd-gui查看deleteById方法调用invoke
     }
+
+
 
     @Test
     public void testProxyImpl(){
