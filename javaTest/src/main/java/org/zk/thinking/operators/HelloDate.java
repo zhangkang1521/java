f5813b0b7bcf8e00caff9db50a5e8998ffb5de7a
zhangkang1521 <zhangkang_pub@163.com>
2017-09-09 18:08:18 +0800

    thinking java

---
+++ javaTest/src/main/java/org/zk/thinking/operators/HelloDate.java
@@ -0,0 +1,16 @@
+package org.zk.thinking.operators;
+
+import static net.mindview.util.Print.*;
+
+/**
+ * Created by Administrator on 9/9/2017.
+ */
+public class HelloDate {
+
+    public static void main(String[] args) {
+        printnb("fuck");
+        printnb("fuck");
+        print("hello");
+        printf("%d is long", 23L);
+    }
+}
